526a0885002b9ad61e770c854a12840bda18f1fa
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-11-30 18:42:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <primaLibreria.h>
 
 int main(){
+  int n;
+  cin >> n;
+  
+  bool funzione (int n);
+  
+  if (funzione==true) cout << "numero primo";
+  else cout << "numero non primo";
   return 0;
 }

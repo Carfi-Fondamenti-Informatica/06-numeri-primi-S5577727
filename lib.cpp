4f2752a283f69141db0c5bc85e82eb3c51f5f7f3
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-01 12:11:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,9 @@
 #include "lib.h"
 
 bool funzione (int n, int i){
-    for ( i; i<n; i++ ){
+    for ( i=2; i<n; i++ ){
         if (n%i == 0){
             return false;
-            break;
         }
     }
     return true;
